17efa85c1aa0a9f68f39cc0badcc5d7c4ab532c3
Adam Cornforth <adam@apcdesigner.com>
2020-05-28 14:20:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Train Departure Pi
+
+Install dependencies with 
+
+`pip install -r requirements.txt`
+
+Run the pygame emulator with
+
+`python ./src/main.py --display pygame --width 256 --height 64`
+
+Example departure board
+
+![img](https://i.redd.it/hu788k5bih421.jpg)
---
+++ src/main.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import time
+import json
+
+try:
+    print("Hello world")
+
+except KeyboardInterrupt:
+    pass
+except ValueError as err:
+    print(f"Error: {err}")
+except KeyError as err:
+    print(f"Error: Please ensure the {err} environment variable is set")
