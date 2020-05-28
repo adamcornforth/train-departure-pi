cc1d524002220eb0e7dddd8fddfacd422a35054a
Adam Cornforth <adam@apcdesigner.com>
2020-05-28 14:21:30 +0100

    Add luma options argument handling

--- src/main.py
+++ src/main.py
@@ -3,8 +3,18 @@ import sys
 import time
 import json
 
+from luma_options import get_device
+from luma.core.render import canvas
+from luma.core.virtual import viewport, snapshot
+
+device = get_device()
+
 try:
-    print("Hello world")
+    # Box and text rendered in portrait mode
+    while True:
+        with canvas(device) as draw:
+            draw.rectangle(device.bounding_box, outline="white", fill="black")
+            draw.text((10, 40), "Hello World", fill="white")
 
 except KeyboardInterrupt:
     pass
