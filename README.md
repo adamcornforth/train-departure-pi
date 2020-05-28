b7b29a4c37004a125e42b38a2111de4a45da5bc9
Adam Cornforth <adam@apcdesigner.com>
2020-05-28 15:55:45 +0100

    Add Dot Matrix font by Daniel Hart
    https://github.com/DanielHartUK/Dot-Matrix-Typeface

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Train Departure Pi
 
+#### Credits:
+
+- Heavily inspired by [UK-Train-Departure-Display](https://github.com/ghostseven/UK-Train-Departure-Display) which forks [train-departure-screen](https://github.com/chrishutchinson/train-departure-screen).
+- Font: [Dot-Matrix-Typeface](https://github.com/DanielHartUK/Dot-Matrix-Typeface) by [Daniel Hart](https://github.com/DanielHartUK).
+- Using the [Luma](https://github.com/rm-hull/luma.core) library which provides a Pillow-compatible drawing canvas for the connected [SSD1322 Display](https://www.aliexpress.com/item/32949282762.html)
+- Used the [luma.examples](https://github.com/rm-hull/luma.examples) repository's [CLI arguments parser](https://github.com/rm-hull/luma.examples/blob/master/examples/demo_opts.py) to initialise the different types of Luma devices.
+
+##  Installation
+
 Install dependencies with 
 
 `pip install -r requirements.txt`
--- src/main.py
+++ src/main.py
@@ -5,16 +5,41 @@ import json
 
 from luma_options import get_device
 from luma.core.render import canvas
+from PIL import ImageFont, Image
 from luma.core.virtual import viewport, snapshot
 
+
+def makeFont(name, size):
+    font_path = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            'fonts',
+            name
+        )
+    )
+    return ImageFont.truetype(font_path, size)
+
+
 device = get_device()
+font = makeFont("Dot Matrix Regular.ttf", 10)
+fontBold = makeFont("Dot Matrix Bold.ttf", 10)
+fontBoldTall = makeFont("Dot Matrix Bold Tall.ttf", 10)
+fontBoldLarge = makeFont("Dot Matrix Bold.ttf", 40)
 
 try:
-    # Box and text rendered in portrait mode
     while True:
+        # The luma.core.render.canvas class automatically creates an PIL.ImageDraw object of the correct dimensions
+        # and bit depth suitable for the device, so you may then call the usual Pillow methods to draw onto the
+        # canvas.
+        #
+        # As soon as the with scope is ended, the resultant image is automatically flushed to the device’s
+        # display memory and the PIL.ImageDraw object is garbage collected.
         with canvas(device) as draw:
             draw.rectangle(device.bounding_box, outline="white", fill="black")
-            draw.text((10, 40), "Hello World", fill="white")
+            draw.text((3, 3), "14:29 London St Pancras          Exp 14:44", fill="yellow", font=font)
+            draw.text((3, 16), "Calling at: Clapham Junction, East Croydon", fill="yellow", font=font)
+            draw.text((3, 29), "3rd 14:51 Moorgate                 On Time", fill="yellow", font=font)
+            draw.text((100, 50), "14:36:00", fill="yellow", font=fontBoldTall)
 
 except KeyboardInterrupt:
     pass
