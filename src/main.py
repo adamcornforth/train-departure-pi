241cb5a63598b9415993fe055f597f55a9e007a8
Adam Cornforth <adam@apcdesigner.com>
2020-06-02 10:49:18 +0100

    Draw some more rows

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import json
 
 from luma_options import get_device
 from luma.core.render import canvas
-from PIL import ImageFont, Image
+from PIL import ImageFont, ImageDraw, Image
 from luma.core.virtual import viewport, snapshot
 
 
@@ -21,16 +21,57 @@ def makeFont(name, size):
     return ImageFont.truetype(font_path, size)
 
 
-def renderClock(draw, width, height):
+def renderClock(draw: ImageDraw, width, height):
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
 
     textwidth, _ = draw.textsize(current_time, fontBoldTall)
     draw.text(((width / 2) - (textwidth / 2), 0), current_time, fill="yellow", font=fontBoldTall)
 
+
+def renderDestinationRow(draw: ImageDraw, width, height):
+    status = "Exp 14:44"
+    departureTime = "14:29"
+    timeWidth, _ = draw.textsize(departureTime, font)
+    statusWidth, _ = draw.textsize(status, font)
+
+    draw.text((0, 0), departureTime, fill="yellow", font=font)
+    draw.text((timeWidth + 5, 0), "London St Pancras", fill="yellow", font=font)
+    draw.text((device.width - statusWidth, 0), status, fill="yellow", font=font)
+
+
+def renderCallingAtRow(draw: ImageDraw, width, height):
+    callingAt = "Calling at:"
+    callingAtWidth, _ = draw.textsize(callingAt, font)
+
+    draw.text((0, 0), callingAt, fill="yellow", font=font)
+    draw.text((callingAtWidth + 5, 0), "Clapham Junction, East Croydon", fill="yellow", font=font)
+
+def renderAdditionalRow(draw: ImageDraw, width, height):
+    nRow = "3rd:"
+    draw.text((0, 0), nRow, fill="yellow", font=font)
+
+    nTime = "14:51"
+    nDestination = "Moorgate"
+    nWidth, _ = draw.textsize(nRow, font)
+    nTimeWidth, _ = draw.textsize(nTime, font)
+    draw.text((nWidth + 5, 0), nTime, fill="yellow", font=font)
+    draw.text((nWidth + nTimeWidth + 10, 0), nDestination, fill="yellow", font=font)
+
+    status = "On time"
+    statusWidth, _ = draw.textsize(status, font)
+    draw.text((device.width - statusWidth, 0), status, fill="yellow", font=font)
+
+
 def drawAll(display):
+    destinationRow = snapshot(device.width, 14, renderDestinationRow, interval=1)
+    callingAtRow = snapshot(device.width, 14, renderCallingAtRow, interval=1)
+    additionalRow = snapshot(device.width, 14, renderAdditionalRow, interval=1)
     clock = snapshot(device.width, 14, renderClock, interval=1)
 
+    display.add_hotspot(destinationRow, (0, 0))
+    display.add_hotspot(callingAtRow, (0, 14))
+    display.add_hotspot(additionalRow, (0, 28))
     display.add_hotspot(clock, (0, 50))
 
     return display
