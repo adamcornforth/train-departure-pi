9eadcaab2c2b130b42c0037ca7e892667df11927
Adam Cornforth <adam@apcdesigner.com>
2020-06-06 16:36:15 +0100

    Try adding sleep back in

--- src/main.py
+++ src/main.py
@@ -108,6 +108,7 @@ try:
     while True:
         with canvas(device, background=board.composition()) as draw:
             board.tick()
+            time.sleep(0.025)
             board.composition.refresh()
 
 except KeyboardInterrupt:
