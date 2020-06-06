b815fa4ca9c2d415140935f8e30399f11963ac5e
Adam Cornforth <adam@apcdesigner.com>
2020-06-06 13:34:52 +0100

    Make board class only concerned about ImageCompositions

--- src/board.py
+++ src/board.py
@@ -5,36 +5,33 @@ from TextImage import TextImage
 from luma.core.image_composition import ImageComposition, ComposableImage
 from PIL import ImageFont, ImageDraw
 
+
 class Board():
     def __init__(self, device, interval=1.0):
         self.device = device
         self.composition = ImageComposition(self.device)
 
-        self.destinationRow = ComposableImage(
-            TextImage(renderDestinationRow, self.device, self.device.width, 14).image,
-            (0, 0)
-        )
-        self.callingAtStations = ComposableImage(
-            TextImage(renderCallingAtStations, self.device, self.device.width * 2, 14).image,
-            (0, 14)
-        )
-        self.callingAt = ComposableImage(
-            TextImage(renderCallingAt, self.device, 40, 14).image,
-            (0, 14)
-        )
-        self.additionalRow = ComposableImage(
-            TextImage(renderAdditionalRow, self.device, self.device.width, 14).image,
-            (0, 28)
-        )
-        self.clockImage = TextImage(renderClock, self.device, self.device.width, 14)
-        self.clock = ComposableImage(
-            self.clockImage.image,
-            (0, 50)
-        )
+        self.compositions = []
+        self.scrollingCompositions = []
+        self.updatingCompositions = []
 
         self.drawComposition()
         self.interval = interval
         self.last_updated = 0.0
+
+    def addRow(self, composableimage: ComposableImage):
+        self.compositions.append(composableimage)
+
+    def addUpdatingRow(self, composableimage: ComposableImage, textimage: TextImage):
+        self.updatingCompositions.append({
+            'composableimage': composableimage,
+            'textimage': textimage
+        })
+        self.addRow(composableimage)
+
+    def addScrollingRow(self, composableimage: ComposableImage):
+        self.scrollingCompositions.append(composableimage)
+        self.addRow(composableimage)
 
     def should_redraw(self):
         """
@@ -43,99 +40,34 @@ class Board():
         return time.monotonic() - self.last_updated > self.interval
 
     def tick(self):
-        if self.should_redraw() == False:
+        if not self.should_redraw():
             return
 
         self.last_updated = time.monotonic()
 
-        self.composition.remove_image(self.destinationRow)
-        self.composition.remove_image(self.callingAtStations)
-        self.composition.remove_image(self.callingAt)
-        self.composition.remove_image(self.additionalRow)
-        self.composition.remove_image(self.clock)
+        for composableimage in self.compositions:
+            self.composition.remove_image(composableimage)
 
-        self.clockImage.update()
-        self.clock = ComposableImage(
-            self.clockImage.image,
-            (0, 50)
-        )
+        for k,updatingimage in enumerate(self.updatingCompositions):
+            updatingimage['textimage'].update()
+            updatingimage['composableimage'].image = ComposableImage(
+                updatingimage['textimage'].image,
+                updatingimage['composableimage'].position,
+                updatingimage['composableimage'].offset
+            ).image
 
-        if self.callingAtStations.offset[0] > self.callingAtStations.width:
-            self.callingAtStations.offset = (0, 0)
-        else:
-            self.callingAtStations.offset = (self.callingAtStations.offset[0] + 1, 0)
+        # Scrolling text
+        for scrollingcomposition in self.scrollingCompositions:
+            if scrollingcomposition.offset[0] > scrollingcomposition.width:
+                scrollingcomposition.offset = (0, 0)
+            else:
+                scrollingcomposition.offset = (scrollingcomposition.offset[0] + 1, 0)
 
         self.drawComposition()
         self.composition.refresh()
         pass
 
     def drawComposition(self):
-        self.composition.add_image(self.destinationRow)
-        self.composition.add_image(self.callingAtStations)
-        self.composition.add_image(self.callingAt)
-        self.composition.add_image(self.additionalRow)
-        self.composition.add_image(self.clock)
+        for composableimage in self.compositions:
+            self.composition.add_image(composableimage)
 
-
-def renderClock(draw: ImageDraw, width, height):
-    t = time.localtime()
-    current_time = time.strftime("%H:%M:%S", t)
-
-    textwidth, _ = draw.textsize(current_time, fontBoldTall)
-    draw.text(((width / 2) - (textwidth / 2), 0), current_time, fill="yellow", font=fontBoldTall)
-
-
-def renderDestinationRow(draw: ImageDraw, width, height):
-    status = "Exp 14:44"
-    departureTime = "14:29"
-    timeWidth, _ = draw.textsize(departureTime, font)
-    statusWidth, _ = draw.textsize(status, font)
-
-    draw.text((0, 0), departureTime, fill="yellow", font=font)
-    draw.text((timeWidth + 5, 0), "London St Pancras", fill="yellow", font=font)
-    draw.text((width - statusWidth, 0), status, fill="yellow", font=font)
-
-
-def renderCallingAt(draw: ImageDraw, width, height):
-    callingAt = "Calling at:"
-
-    draw.text((0, 0), callingAt, fill="yellow", font=font)
-
-
-def renderCallingAtStations(draw: ImageDraw, width, height):
-    callingAt = "Calling at:"
-    callingAtWidth = draw.textsize(callingAt, font)[0]
-    callingAtStations = "Clapham Junction, East Croydon, Blackfriars, London, London St Pancras"
-
-    draw.text((callingAtWidth, 0), callingAtStations, fill="yellow", font=font)
-
-
-def renderAdditionalRow(draw: ImageDraw, width, height):
-    nRow = "3rd:"
-    draw.text((0, 0), nRow, fill="yellow", font=font)
-
-    nTime = "14:51"
-    nDestination = "Moorgate"
-    nWidth, _ = draw.textsize(nRow, font)
-    nTimeWidth, _ = draw.textsize(nTime, font)
-    draw.text((nWidth + 5, 0), nTime, fill="yellow", font=font)
-    draw.text((nWidth + nTimeWidth + 10, 0), nDestination, fill="yellow", font=font)
-
-    status = "On time"
-    statusWidth, _ = draw.textsize(status, font)
-    draw.text((width - statusWidth, 0), status, fill="yellow", font=font)
-
-def makeFont(name, size):
-    font_path = os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__),
-            'fonts',
-            name
-        )
-    )
-    return ImageFont.truetype(font_path, size)
-
-font = makeFont("Dot Matrix Regular.ttf", 10)
-fontBold = makeFont("Dot Matrix Bold.ttf", 10)
-fontBoldTall = makeFont("Dot Matrix Bold Tall.ttf", 10)
-fontBoldLarge = makeFont("Dot Matrix Bold.ttf", 40)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,78 @@
 import os
-import sys
 import time
-
-import json
 
 from board import Board
 from luma_options import get_device
+from TextImage import TextImage
+from luma.core.image_composition import ComposableImage
+from PIL import ImageFont, ImageDraw
 from luma.core.render import canvas
+
+
+def renderDestinationRow(draw: ImageDraw, width, height):
+    status = "Exp 14:44"
+    departureTime = "14:29"
+    timeWidth, _ = draw.textsize(departureTime, font)
+    statusWidth, _ = draw.textsize(status, font)
+
+    draw.text((0, 0), departureTime, fill="yellow", font=font)
+    draw.text((timeWidth + 5, 0), "London St Pancras", fill="yellow", font=font)
+    draw.text((width - statusWidth, 0), status, fill="yellow", font=font)
+
+
+def renderCallingAt(draw: ImageDraw, width, height):
+    callingAt = "Calling at:"
+
+    draw.text((0, 0), callingAt, fill="yellow", font=font)
+
+
+def renderCallingAtStations(draw: ImageDraw, width, height):
+    callingAt = "Calling at:"
+    callingAtWidth = draw.textsize(callingAt, font)[0]
+    callingAtStations = "Clapham Junction, East Croydon, Blackfriars, London, London St Pancras"
+
+    draw.text((callingAtWidth, 0), callingAtStations, fill="yellow", font=font)
+
+
+def renderAdditionalRow(draw: ImageDraw, width, height):
+    nRow = "3rd:"
+    draw.text((0, 0), nRow, fill="yellow", font=font)
+
+    nTime = "14:51"
+    nDestination = "Moorgate"
+    nWidth, _ = draw.textsize(nRow, font)
+    nTimeWidth, _ = draw.textsize(nTime, font)
+    draw.text((nWidth + 5, 0), nTime, fill="yellow", font=font)
+    draw.text((nWidth + nTimeWidth + 10, 0), nDestination, fill="yellow", font=font)
+
+    status = "On time"
+    statusWidth, _ = draw.textsize(status, font)
+    draw.text((width - statusWidth, 0), status, fill="yellow", font=font)
+
+
+def renderClock(draw: ImageDraw, width, height):
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+
+    textwidth, _ = draw.textsize(current_time, fontBoldTall)
+    draw.text(((width / 2) - (textwidth / 2), 0), current_time, fill="yellow", font=fontBoldTall)
+
+
+def makeFont(name, size):
+    font_path = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            'fonts',
+            name
+        )
+    )
+    return ImageFont.truetype(font_path, size)
+
+
+font = makeFont("Dot Matrix Regular.ttf", 10)
+fontBold = makeFont("Dot Matrix Bold.ttf", 10)
+fontBoldTall = makeFont("Dot Matrix Bold Tall.ttf", 10)
+fontBoldLarge = makeFont("Dot Matrix Bold.ttf", 40)
 
 try:
     device = get_device()
@@ -17,6 +83,30 @@ try:
     time.tzset()
 
     board = Board(device, interval)
+
+    board.addRow(ComposableImage(
+        TextImage(renderDestinationRow, device, device.width, 14).image,
+        (0, 0)
+    ))
+    board.addScrollingRow(ComposableImage(
+        TextImage(renderCallingAtStations, device, device.width * 2, 14).image,
+        (0, 14)
+    ))
+    board.addRow(ComposableImage(
+        TextImage(renderCallingAt, device, 40, 14).image,
+        (0, 14)
+    ))
+    board.addRow(ComposableImage(
+        TextImage(renderAdditionalRow, device, device.width, 14).image,
+        (0, 28)
+    ))
+    image = TextImage(renderClock, device, device.width, 14)
+    board.addUpdatingRow(ComposableImage(
+        image.image,
+        (0, 50)
+    ), image)
+
+    board.drawComposition()
 
     while True:
         with canvas(device, background=board.composition()) as draw:
