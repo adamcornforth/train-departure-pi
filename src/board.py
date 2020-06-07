8fd032dca21a1d150ac57f7e48215c7ef234e35e
Adam Cornforth <adam@apcdesigner.com>
2020-06-07 21:28:33 +0100

    Lint

--- src/board.py
+++ src/board.py
@@ -5,7 +5,7 @@ from TextImage import TextImage
 from luma.core.image_composition import ImageComposition, ComposableImage
 
 
-class Board():
+class Board:
     def __init__(self, device, interval=1.0):
         self.device = device
         self.composition = ImageComposition(self.device)
@@ -16,15 +16,16 @@ class Board():
         self.last_updated = 0.0
         self.last_delay_tick = 0.0
 
-    def addRow(self,
-               textimage: TextImage,
-               position: tuple = (0,0),
-               offset: tuple = (0, 0),
-               scrolling=False,
-               direction="h",
-               delay=0,
-               initialdelay=None
-        ):
+    def addRow(
+        self,
+        textimage: TextImage,
+        position: tuple = (0, 0),
+        offset: tuple = (0, 0),
+        scrolling=False,
+        direction="h",
+        delay=0,
+        initialdelay=None,
+    ):
         """
         Add a row to paint on every Board tick
         """
@@ -33,14 +34,16 @@ class Board():
         if not initialdelay:
             initialdelay = delay
 
-        self.compositions.append({
-            'composableimage': composableimage,
-            'textimage': textimage,
-            'scrolling': scrolling,
-            'direction': direction,
-            'delay': delay,
-            'initialdelay': initialdelay
-        })
+        self.compositions.append(
+            {
+                "composableimage": composableimage,
+                "textimage": textimage,
+                "scrolling": scrolling,
+                "direction": direction,
+                "delay": delay,
+                "initialdelay": initialdelay,
+            }
+        )
 
         self.composition.add_image(composableimage)
 
@@ -56,31 +59,37 @@ class Board():
     def tick(self):
         if self.should_tick_delay():
             for composableimage in self.compositions:
-                if composableimage['scrolling'] and composableimage['delay']:
-                    composableimage['delay'] = composableimage['delay'] - 1
+                if composableimage["scrolling"] and composableimage["delay"]:
+                    composableimage["delay"] = composableimage["delay"] - 1
             self.last_delay_tick = time.monotonic()
 
         for composableimage in self.compositions:
-            if composableimage['scrolling'] and not composableimage['delay']:
-                if composableimage['direction'] == "v":
+            if composableimage["scrolling"] and not composableimage["delay"]:
+                if composableimage["direction"] == "v":
                     # Scrolling rows need their offsets incrementing every tick
-                    if composableimage['composableimage'].offset[1] > composableimage['composableimage'].height:
-                        composableimage['composableimage'].offset = (0, 0)
-                        composableimage['delay'] = composableimage.get('initialdelay')
+                    if (
+                        composableimage["composableimage"].offset[1]
+                        > composableimage["composableimage"].height
+                    ):
+                        composableimage["composableimage"].offset = (0, 0)
+                        composableimage["delay"] = composableimage.get("initialdelay")
                     else:
-                        composableimage['composableimage'].offset = (
+                        composableimage["composableimage"].offset = (
                             0,
-                            composableimage['composableimage'].offset[1] + 1,
+                            composableimage["composableimage"].offset[1] + 1,
                         )
                 else:
                     # Scrolling rows need their offsets incrementing every tick
-                    if composableimage['composableimage'].offset[0] > composableimage['composableimage'].width:
-                        composableimage['composableimage'].offset = (0, 0)
-                        composableimage['delay'] = composableimage.get('initialdelay')
+                    if (
+                        composableimage["composableimage"].offset[0]
+                        > composableimage["composableimage"].width
+                    ):
+                        composableimage["composableimage"].offset = (0, 0)
+                        composableimage["delay"] = composableimage.get("initialdelay")
                     else:
-                        composableimage['composableimage'].offset = (
-                            composableimage['composableimage'].offset[0] + 1,
-                            0
+                        composableimage["composableimage"].offset = (
+                            composableimage["composableimage"].offset[0] + 1,
+                            0,
                         )
 
         """
@@ -90,18 +99,18 @@ class Board():
             return
 
         for updatingimage in self.compositions:
-            if updatingimage['textimage'].should_redraw():
-                updatingimage['textimage'].update()
+            if updatingimage["textimage"].should_redraw():
+                updatingimage["textimage"].update()
                 self.compositions.remove(updatingimage)
-                self.composition.remove_image(updatingimage['composableimage'])
+                self.composition.remove_image(updatingimage["composableimage"])
                 self.addRow(
-                    updatingimage['textimage'],
-                    updatingimage['composableimage'].position,
-                    updatingimage['composableimage'].offset,
-                    updatingimage['scrolling'],
-                    updatingimage['direction'],
-                    updatingimage['delay'],
-                    updatingimage['initialdelay']
+                    updatingimage["textimage"],
+                    updatingimage["composableimage"].position,
+                    updatingimage["composableimage"].offset,
+                    updatingimage["scrolling"],
+                    updatingimage["direction"],
+                    updatingimage["delay"],
+                    updatingimage["initialdelay"],
                 )
 
         self.last_updated = time.monotonic()
--- src/main.py
+++ src/main.py
@@ -12,12 +12,12 @@ from luma.core.render import canvas
 
 def renderDestinationRow(draw: ImageDraw, width, height):
     status = "On time"
-    departureTime = data['departures']['all'][0]['aimed_departure_time']
+    departureTime = data["departures"]["all"][0]["aimed_departure_time"]
     timeWidth, _ = draw.textsize(departureTime, font)
     statusWidth, _ = draw.textsize(status, font)
 
     draw.text((0, 0), departureTime, fill="yellow", font=font)
-    destination = data['departures']['all'][0]['destination_name']
+    destination = data["departures"]["all"][0]["destination_name"]
     draw.text((timeWidth + 5, 0), destination, fill="yellow", font=font)
     draw.text((width - statusWidth, 0), status, fill="yellow", font=font)
 
@@ -36,8 +36,8 @@ def renderAdditionalRow(draw: ImageDraw, width, height):
     nRow = "2nd:"
     draw.text((0, 0), nRow, fill="yellow", font=font)
 
-    nTime = data['departures']['all'][1]['aimed_departure_time']
-    nDestination = data['departures']['all'][1]['destination_name']
+    nTime = data["departures"]["all"][1]["aimed_departure_time"]
+    nDestination = data["departures"]["all"][1]["destination_name"]
     nWidth, _ = draw.textsize(nRow, font)
     nTimeWidth, _ = draw.textsize(nTime, font)
     draw.text((nWidth + 5, 0), nTime, fill="yellow", font=font)
@@ -52,8 +52,8 @@ def renderAdditionalRow3(draw: ImageDraw, width, height):
     nRow = "3rd:"
     draw.text((0, 0), nRow, fill="yellow", font=font)
 
-    nTime = data['departures']['all'][2]['aimed_departure_time']
-    nDestination = data['departures']['all'][2]['destination_name']
+    nTime = data["departures"]["all"][2]["aimed_departure_time"]
+    nDestination = data["departures"]["all"][2]["destination_name"]
     nWidth, _ = draw.textsize(nRow, font)
     nTimeWidth, _ = draw.textsize(nTime, font)
     draw.text((nWidth + 5, 0), nTime, fill="yellow", font=font)
@@ -69,17 +69,16 @@ def renderClock(draw: ImageDraw, width, height):
     current_time = time.strftime("%H:%M:%S", t)
 
     textwidth, _ = draw.textsize(current_time, fontBoldTall)
-    draw.text(((width / 2) - (textwidth / 2), 0), current_time, fill="yellow", font=fontBoldTall)
+    draw.text(
+        ((width / 2) - (textwidth / 2), 0),
+        current_time,
+        fill="yellow",
+        font=fontBoldTall,
+    )
 
 
 def makeFont(name, size):
-    font_path = os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__),
-            'fonts',
-            name
-        )
-    )
+    font_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "fonts", name))
     return ImageFont.truetype(font_path, size)
 
 
@@ -93,11 +92,11 @@ try:
     # Time between redraws on the display
     interval = 0.025
 
-    os.environ['TZ'] = 'Europe/London'
+    os.environ["TZ"] = "Europe/London"
     time.tzset()
 
-    API_ID = os.environ.get('API_ID')
-    API_KEY = os.environ.get('API_KEY')
+    API_ID = os.environ.get("API_ID")
+    API_KEY = os.environ.get("API_KEY")
 
     if not API_ID or not API_KEY:
         raise EnvironmentError("API_ID or API_KEY environment variables not set!")
@@ -113,52 +112,50 @@ try:
     # with open(os.path.dirname(os.path.realpath(__file__)) + '/departures.json') as json_file:
     #     data = json.load(json_file)
 
-    response = requests.get(
-        data['departures']['all'][0]["service_timetable"]["id"]
-    )
+    response = requests.get(data["departures"]["all"][0]["service_timetable"]["id"])
     timetable = json.loads(response.text)
     # with open(os.path.dirname(os.path.realpath(__file__)) + '/timetable.json') as json_file:
     #     timetable = json.load(json_file)
-    timetable_stops = ", ".join([stop['station_name'] for stop in timetable['stops'][1:-1]])
+    timetable_stops = ", ".join(
+        [stop["station_name"] for stop in timetable["stops"][1:-1]]
+    )
 
     board = Board(device, interval)
 
-    board.addRow(
-        TextImage(renderDestinationRow, device, device.width, 14, 10)
-    )
-    if len(data['departures']['all']):
+    board.addRow(TextImage(renderDestinationRow, device, device.width, 14, 10))
+    if len(data["departures"]["all"]):
         with canvas(device) as draw:
             board.addRow(
-                TextImage(renderCallingAtStations, device, draw.textsize(timetable_stops, font)[0], 14, 10),
+                TextImage(
+                    renderCallingAtStations,
+                    device,
+                    draw.textsize(timetable_stops, font)[0],
+                    14,
+                    10,
+                ),
                 (draw.textsize("Calling at:", font)[0], 14),
                 scrolling=True,
                 direction="h",
-                delay=2
+                delay=2,
             )
-        board.addRow(
-            TextImage(renderCallingAt, device, 40, 14, 10),
-            (0, 14)
-        )
-    if len(data['departures']['all']) > 2:
+        board.addRow(TextImage(renderCallingAt, device, 40, 14, 10), (0, 14))
+    if len(data["departures"]["all"]) > 2:
         board.addRow(
             TextImage(renderAdditionalRow3, device, device.width, 14, 4.5),
             (0, 28),
             scrolling=True,
             direction="v",
-            delay=5
+            delay=5,
         )
-    if len(data['departures']['all']) > 1:
+    if len(data["departures"]["all"]) > 1:
         board.addRow(
             TextImage(renderAdditionalRow, device, device.width, 14, 10),
             (0, 28),
-            scrolling=(len(data['departures']['all']) > 2),
+            scrolling=(len(data["departures"]["all"]) > 2),
             direction="v",
-            delay=5
+            delay=5,
         )
-    board.addRow(
-        TextImage(renderClock, device, device.width, 14, 1),
-        (0, 50)
-    )
+    board.addRow(TextImage(renderClock, device, device.width, 14, 1), (0, 50))
 
     while True:
         with canvas(device, background=board.composition()) as draw:
